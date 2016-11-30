a491af21767936579324ee3c1d49e70e75e26a86
Geril <michal.slabej94@gmail.com>
2016-11-30 14:39:55 +0100

    Solution for OddOccurrencesInArray added

---
+++ arrays/oddOccurrencesInArray/solution.js
@@ -0,0 +1,11 @@
+function solution(A) {
+    let returnValue;
+    const sorted = A.sort();
+    for(let i = 0; i < sorted.length; i++) {
+    	if(i % 2 === 0 && sorted[i+1] !== sorted[i]) {
+            returnValue = sorted[i];
+            break;
+        }
+    }
+    return returnValue;
+}
