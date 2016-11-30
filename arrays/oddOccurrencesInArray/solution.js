d27f32534bbb2baba29806aa0d5079676167f198
Geril <michal.slabej94@gmail.com>
2016-11-30 14:44:05 +0100

    coding style changes in OddOccurrencesInArray task

--- arrays/oddOccurrencesInArray/solution.js
+++ arrays/oddOccurrencesInArray/solution.js
@@ -1,8 +1,8 @@
 function solution(A) {
     let returnValue;
     const sorted = A.sort();
-    for(let i = 0; i < sorted.length; i++) {
-    	if(i % 2 === 0 && sorted[i+1] !== sorted[i]) {
+    for (let i = 0; i < sorted.length; i++) {
+        if (i % 2 === 0 && sorted[i+1] !== sorted[i]) {
             returnValue = sorted[i];
             break;
         }
